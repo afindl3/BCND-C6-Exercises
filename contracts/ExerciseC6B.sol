03766f03ec7d70ff789a76cc738c4bc913abc8f3
afindl3 <alisonfindlay93@hotmail.com>
2020-04-27 18:10:03 -0400

    Exercise C6B complete - Lesson 3 Receive, Transfer, and Send Funds

--- contracts/ExerciseC6B.sol
+++ contracts/ExerciseC6B.sol
@@ -6,47 +6,72 @@ pragma solidity ^0.4.25;
 
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-
 contract ExerciseC6B {
-    using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
+  using SafeMath for uint256; // Allow SafeMath functions to be called for all uint256 types (similar to "prototype" in Javascript)
 
-    /********************************************************************************************/
-    /*                                       DATA VARIABLES                                     */
-    /********************************************************************************************/
+  /********************************************************************************************/
+  /*                                       DATA VARIABLES                                     */
+  /********************************************************************************************/
 
+  address private contractOwner;                  // Account used to deploy contract
+  mapping (address => uint256) private sales;      // Lesson 3.6 - Checks-Effects-Iteractions
+  uint256 private enabled = block.timestamp;      // Lesson 3.7 - Rate Limiting
+  uint256 private counter = 1;                    // Lesson 3.8 - Re-Entrancy Guard
 
-    address private contractOwner;                  // Account used to deploy contract
+  constructor() public {
+    contractOwner = msg.sender;
+  }
+  
+  /********************************************************************************************/
+  /*                                       FUNCTION MODIFIERS                                 */
+  /********************************************************************************************/
 
+  // Modifiers help avoid duplication of code. They are typically used to validate something
+  // before a function is allowed to be executed.
 
-    constructor
-                (
-                )
-                public 
-    {
-        contractOwner = msg.sender;
-    }
-   
-    /********************************************************************************************/
-    /*                                       FUNCTION MODIFIERS                                 */
-    /********************************************************************************************/
+  /**
+  * @dev Modifier that requires the "ContractOwner" account to be the function caller
+  */
+  modifier requireContractOwner() {
+    require(msg.sender == contractOwner, "Caller is not contract owner");
+    _;
+  }
 
-    // Modifiers help avoid duplication of code. They are typically used to validate something
-    // before a function is allowed to be executed.
+  // Lesson 3.7 - Rate Limiting
+  modifier rateLimit(uint time) {
+    require(block.timestamp >= enabled, "Rate limiting in effect");
+    enabled = enabled.add(time);
+    _;
+  }
 
-    /**
-    * @dev Modifier that requires the "ContractOwner" account to be the function caller
-    */
-    modifier requireContractOwner()
-    {
-        require(msg.sender == contractOwner, "Caller is not contract owner");
-        _;
-    }
+  // Lesson 3.8 - Re-Entrancy Guard
+  modifier entrancyGuard() {
+    counter = counter.add(1);
+    uint256 guard = counter;
+    _;
+    require(guard == counter, "That is not allowed"); // If a function was called repeatedly, counter would be greater than guard
+  }
 
-    /********************************************************************************************/
-    /*                                     SMART CONTRACT FUNCTIONS                             */
-    /********************************************************************************************/
+  /********************************************************************************************/
+  /*                                     SMART CONTRACT FUNCTIONS                             */
+  /********************************************************************************************/
 
+  // Lesson 3.6 - Checks-Effects-Iteractions
+  // Write a function safeWithdraw(uint256) that protects against re-entrancy attacks using
+  // the Checks-Effects-Interactions pattern
+  function safeWithdraw(uint256 amount) external entrancyGuard() {
+    // Checks - Verify caller is an externally owned account
+    require(msg.sender == tx.origin, "Contracts not allowed");
+    //Checks - Verify caller has adequate funds to withdraw
+    require(sales[msg.sender] >= amount, "Insufficient funds");
 
-    
+    uint256 balance = sales[msg.sender];
+    // Effects - Reset sales for caller address to zero
+    sales[msg.sender] = sales[msg.sender].sub(balance);
+
+    // Interaction - Transfer value of sales for caller to caller address
+    msg.sender.transfer(balance);
+  }
+  
 }
 
--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic = "sadness pottery make bachelor noble awful quantum because shoot ticket tissue certain";
 
 module.exports = {
   networks: {
